044f7bb589bb05f253a9eda71e7d80bab0696a35
AchPaudel <achpaudel@ku.edu>
2022-06-06 12:53:59 -0500

    instructions updated

--- README.txt
+++ README.txt
@@ -9,8 +9,15 @@ Install command:
 	- flask run
 
 4) on a web browser, navigate to 127.0.0.1 and use port 5000
+
+PostsAPI:
    so to ping the api, go to: 127.0.0.1:5000/api/ping
    for the posts, heres an example: 127.0.0.1:5000/api/posts?tags=history,tech&sortBy=reads
 
-5) For the test aspect, if the sort is being sorted correctly, then the console will print out either
+   To test... if the sort is being sorted correctly, then the console will print out either
    "Test passed!" with some details of the sort, or itll output "Test failed!".
+
+Recipes:
+	127.0.0.1:5000/recipes
+	127.0.0.1:5000/recipes/details/<recipe>
+	Add recipes and Update Recipes: Look at cmds.txt
--- RecipesAPI/proj.py
+++ RecipesAPI/proj.py
@@ -1,139 +1,84 @@
-from os import remove
-from unittest.util import sorted_list_difference
-from parso import parse
-import requests
-import json
 from unicodedata import numeric
 from webbrowser import get
-from flask import Flask, json, request, url_for
-from urllib import parse as urlparse
+from flask import Flask, json, request
 
 app = Flask(__name__)
-app.debug = True
 
-##TESTING Fetch
-def getTagJSON(tagParam):
-    response_API = requests.get("https://api.hatchways.io/assessment/blog/posts%s" % tagParam)
-    data = json.loads(response_API.text)
-    return data
-
-def toYesQ(query):
-    for i in range(len(query)):
-        query[i] = '?' + query[i]
-    return query
-
-def getTagsDict(queriesNoQ):
-    tagDict = dict()
-    for i in queriesNoQ:
-        i = i.split('=')
-        a = i[0][1:]
-        if(a == 'tags'):
-            tagsList = i[1].split(',')
-            if(len(tagsList) > 1):
-                b = tagsList
-            else:
-                b = i[1]
-        else:
-            b = i[1]
-        tagDict[a] = b
-    return tagDict
-        
-
-@app.route('/api/ping', methods=['GET'])
-def route1():
-    returnDict = dict()
-    returnDict["success"] = True
-    return json.dumps(returnDict, indent=4)
-        
-
-def removeDuplicates(toSort):
-    removeDuplicatesID = list()
-    for i in toSort:
-        id = i['id']
-        if(id not in removeDuplicatesID):
-            removeDuplicatesID.append(id)
-            
-
-    res = list()
-    for ids in removeDuplicatesID:
-        for i in toSort:
-            if(i['id'] == ids):
-                res.append(i)
-    return res
-
-def testFunc(result, sortBy, direction):
-    checkSort = list()
-    iter = 0
-    checkSort.append(result[0][sortBy])
-    for i in result:
-        iter += 1
-        checkSort.append(i[sortBy])
-        if(iter > 0):
-            if(direction == 'asc'):
-                if(checkSort[iter] < checkSort[iter-1]):
-                    return False
-            if(direction == 'desc'):
-                if(checkSort[iter] > checkSort[iter-1]):
-                    return False
-    print()
-    print("Test Passed!", "\nSorted by:", sortBy, "\nDirection:", direction)
-    print()
-    return True
+f = open('data.json', 'r')
+oldData = json.load(f)
+recipesList = oldData['recipes']
 
 
-@app.route('/api/posts', methods=['GET'])
-def routeMe():
-    getTag = request.args.get('tags')
-    getSortBy = request.args.get('sortBy', default='id')
-    getDirection = request.args.get('direction', default='asc')
+@app.route('/recipes', methods=['GET'])
+def getAllRecipes():
+    namesList = []
+    for i in recipesList:
+        namesList.append(i['name'])
+    toDict = dict()
+    toDict['recipeNames'] = namesList
+    return json.dumps(toDict, indent=4)
 
-    queryDict = dict()
-    queryDict['tags'] = getTag
-    queryDict['sortBy'] = getSortBy
-    queryDict['direction'] = getDirection
 
-    #Parse if there are multiple tags to fetch
-    tagsList = list(queryDict['tags'].split(','))
-    if(len(tagsList) > 1):
-        queryDict['tags'] = tagsList
 
-    #Check if there are multiple queries
-    isList = True
-    if (type(queryDict['tags']) == type('test')):
-        isList = False
 
-    #Fetch from the API and store into dictionary
-    getJsonResult = dict()
-    getJsonResult['posts'] = list()
-    if(isList):
-        for i in queryDict['tags']:
-            getJsonResult['posts'].append(getTagJSON('?tag=' + i).get('posts'))
-        tempList = list()
-        for i in getJsonResult['posts']:
-            for j in i:
-                tempList.append(j)
-        getJsonResult['posts'] = tempList
-        json.dump(getJsonResult['posts'], open('posts.json', 'w'), indent=4)
-    else:
-        getJsonResult['posts'] = list((getTagJSON('?tag=' + queryDict['tags']).get('posts')))
+@app.route('/recipes/details/<recipe>', methods=['GET'])
+def getRecipe(recipe):
+    getName = '%s' % recipe
+    numDict = dict()
+    for i in recipesList:
+        if(i['name'] == getName):
+            #Number of steps
+            numCount = len(i['instructions'])
+            numDict["numSteps"] = numCount
+
+            #Get ingredients
+            jsonnewDict = dict()
+            jsonnewDict["details"] = dict()
+            jsonnewDict["details"]['ingredients'] = i['ingredients']
+            jsonnewDict["details"].update(numDict)
+            return json.dumps(jsonnewDict, indent=4)
+
+
+@app.route('/recipes', methods=['POST'])
+def addRecipe():
+    data = json.loads(request.data)
+    #Check if recipe already exists
+    for i in recipesList:
+        if(i['name'] == data['name']):
+            return json.dumps({"error": "Recipe already exists"}, indent=4)
     
-
-    #Sort the dictionary of results
-    if (getSortBy=='popularity' or getSortBy=='reads' or getSortBy=='likes' or getSortBy=='id'):
-        if(getDirection=='desc'):
-            sortedReturn = sorted(getJsonResult['posts'], key= lambda x: x[getSortBy], reverse=True)
-        elif(getDirection=='asc'):
-            sortedReturn = sorted(getJsonResult['posts'], key= lambda x: x[getSortBy])            
-        else:
-            return json.dumps({"direction parameter is invalid."})
-    else:
-        return json.dumps({"sortBy parameter is invalid."})
-
-    cleanList = removeDuplicates(sortedReturn)
-    if(testFunc(cleanList, getSortBy, getDirection)):
-        return json.dumps({"posts":cleanList}, indent=4)
-    else:
-        print("Test Failed!")
+    #Get new recipe data
+    newRecipe = dict()
+    newRecipe['name'] = data['name']
+    newRecipe['ingredients'] = data['ingredients']
+    newRecipe['instructions'] = data['instructions']
+    #Add new recipe to list
+    recipesList.append(newRecipe)
+    oldData['recipes'] = recipesList
+    return json.dumps(recipesList, indent=4)
 
 
-    
+
+@app.route('/recipes', methods=["PUT"])
+def updateRecipe():
+    data = json.loads(request.data)
+    #Add recipe to list
+    newRecipe = dict()
+    newRecipe['name'] = data['name']
+    newRecipe['ingredients'] = data['ingredients']
+    newRecipe['instructions'] = data['instructions']
+
+    for i in recipesList:
+        if(i['name'] == newRecipe['name']):
+            i['ingredients'] = newRecipe['ingredients']
+            i['instructions'] = newRecipe['instructions']
+            oldData['recipes'] = recipesList
+            return json.dumps(recipesList, indent=4)
+
+    return json.dumps({"error":"Recipe does not exist"}, indent=4)
+
+
+
+
+
+
